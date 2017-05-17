718fa1da2c4a5255fe8f44fce9d1a614e18f8ec6
Andy Davies <1andydavies1@gmail.com>
2017-05-17 13:23:58 +0100

    Watch for scss changes automatically

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@ var gulp = require('gulp');
 var source = '_sass/**/*.scss';
 var destination = '_gulped-sass';
 
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
+gulp.task('default', function() {
+	gulp.watch(source, ['autoprefixer']);
+});
 
 gulp.task('autoprefixer', function () {
+
+	var postcss = require('gulp-postcss');
+	var autoprefixer = require('autoprefixer');
+
 	return gulp.src(source)
     	.pipe(postcss([autoprefixer({
     		browsers: ['last 2 versions']
